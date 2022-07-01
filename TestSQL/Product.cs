e9eb5787222bea20c8ae6089afab93665067b1e4
СемёнТихонов <@LAPTOP-G0BRQBKQ>
2022-07-01 16:40:45 +0300

    testSQL

---
+++ TestSQL/Product.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProductType> ProductTypes { get; set; } = new List<ProductType>();
+    }
+
+    public class ProductType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ TestSQL/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp5;
+using Microsoft.EntityFrameworkCore;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+    Product apple = new Product { Name = "apple" };
+    Product orange = new Product { Name = "orange" };
+    db.Products.AddRange(apple, orange);
+
+    ProductType fruit = new ProductType { Name = "fruit" };
+   
+    db.ProductTypes.AddRange(fruit);
+    apple.ProductTypes.Add(fruit);
+
+    db.SaveChanges();
+
+    var productsType = db.Products.Include(c => c.ProductTypes).ToList();
+    foreach (var item in productsType)
+    {
+        Console.WriteLine(item.Name);
+        foreach (var item1 in item.ProductTypes)
+        {
+            Console.WriteLine(item1.Name);
+        }
+        Console.WriteLine("--------------------");
+    }
+}
